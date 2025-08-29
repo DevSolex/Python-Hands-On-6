53cedbd59b2b8ecdb56073ff9ebf4136435962a3
DevSolex <devsolex6@gmail.com>
2025-08-29 16:11:51 +0100

    solve task 3

--- task3.py
+++ task3.py
@@ -18,3 +18,21 @@ print(calculate_grade(60))   # D
 print(calculate_grade(70))   # C
 print(calculate_grade(89))   # B
 """
+
+student_score = int(input('Enter score:\n>>'))
+def calculate_grade(score):
+	if 90 <= score <= 100:
+		return 'A'
+	elif 80 <= score <= 89:
+		return 'B'
+	elif 70 <= score <= 79:
+		return 'C'
+	elif 60 <= score <= 69:
+		return 'D'
+	elif 0 <= score <= 60:
+		return 'F'
+	else:
+		return 'INVALID INPUT!'
+
+
+print(calculate_grade(student_score))
